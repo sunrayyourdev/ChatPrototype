e4592dfa65a9f97a045dada14b517571dfe9d95f
Jake Onquit <onquitjake@gmail.com>
2025-07-14 11:44:59 +0800

    change views

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Helo Liz")
+                .padding()
+                .background()
+            Text("Cello melo")
         }
         .padding()
     }
