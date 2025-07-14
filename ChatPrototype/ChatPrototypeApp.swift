28bf17da9f3ecb2c7b574483b3904ec4f5d0703a
Jake Onquit <onquitjake@gmail.com>
2025-07-14 09:49:01 +0800

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Guest User on 14/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
