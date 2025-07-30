cb53f1fe320d89ce4754f702a6604c7ea4c6ec46
Jake Onquit <onquitjake@gmail.com>
2025-07-30 09:22:20 +0800

    edit content view

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -2,18 +2,25 @@
 //  ContentView.swift
 //  ChatPrototype
 //
-//  Created by Jake Onquit on 30/7/25.
+//  Created by Guest User on 14/7/25.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 10) {
+            Text("Hello World!")
+                .font(.title)
+                .shadow(radius: 8)
+                .padding(10)
+            Text("Some of my favourite things")
+                .padding(10)
+            HStack(spacing: 10) {
+                Image(systemName: "book")
+                Image(systemName: "headphones")
+                Image(systemName: "fish")
+            }
         }
         .padding()
     }
@@ -22,3 +29,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
