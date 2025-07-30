5999ab05ad46a3b54e848e330407b49c7de93913
Jake Onquit <onquitjake@gmail.com>
2025-07-30 09:17:23 +0800

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Jake Onquit on 30/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatPrototypeTests/ChatPrototypeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeTests.swift
+//  ChatPrototypeTests
+//
+//  Created by Jake Onquit on 30/7/25.
+//
+
+import Testing
+@testable import ChatPrototype
+
+struct ChatPrototypeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
